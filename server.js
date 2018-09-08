98531d395c3fdfe0c0e27b5d2400d2465e6c0614
Alfredo Flores <alfredo@appfactorie.com>
2018-09-09 01:47:30 +0800

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/your_app_name'));
+app.use(express.static(__dirname + '/dist/SaleIT'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
+res.sendFile(path.join(__dirname+'/dist/SaleIT/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
